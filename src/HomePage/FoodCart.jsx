bda8d9adeab6fb00765f04e1fbe6c5e963369732
Tengis <tengis20040616@gmail.com>
2022-09-19 00:23:23 +0800

    modal hiij duusgah

--- src/HomePage/FoodCart.jsx
+++ src/HomePage/FoodCart.jsx
@@ -1,10 +1,28 @@
 import { Box, Typography, Button } from '@mui/material';
 import FoodImg from "../images/addFoodImg.png"
-import { useFunction } from '../provider/Function';
+import { useState} from 'react';
+import {Modal} from '@mui/material';
+import Img from "../images/Img.png"
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import {Input} from '@mui/material';
 
+const style = {
+    width:700,
+    height:1200,
+    bgcolor: 'background.paper',
+    border: '1px solid #000',
+    boxShadow: 24,
+    borderRadius:"20px",
+    display:"flex",
+    flexDirection:"column"
+  };
 
 const FoodCart = ({ imgSrc, title, portion, price, isAdd = true }) => {
-    const {handleOpen} = useFunction();
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <Box container sx={{height:"30vh", width:"13vw", backgroundColor:"yellow", display:"flex",justifyContent:"space-around", alignItems:"center", flexDirection:"column"}}>
             <Box>
@@ -18,9 +36,38 @@ const FoodCart = ({ imgSrc, title, portion, price, isAdd = true }) => {
                         <Typography>{price}</Typography>
                         <Button></Button>
                     </Box> :
-                    <Button sx={{borderRadius:"100%", height:"60px", width:"50px", fontSize:"25px"}} variant="contained" color="success">+</Button>
+                    <Button onClick={handleOpen} sx={{borderRadius:"100%", height:"60px", width:"50px", fontSize:"25px"}} variant="contained" color="success">+</Button>
                 }
             </Box>
+            <Box>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Box sx={{width:"100%", height:"5vh",display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-around", borderBottom:"1px solid silver"}}>
+                            <Button variant='contained' color='error'>X</Button>
+                            <Typography variant='h5'>Хоол нэмэх</Typography>
+                            <Button variant='contained' color='success'>Хадгалах</Button>
+                        </Box>
+                        <Box sx={{width:"100%", height:"20vh", backgroundColor:"yellow", display:"flex", flexDirection:"row"}}>
+                            <Box sx={{width:"40%", height:"100%"}}>
+                                <img src={Img}/>
+                                <CameraAltIcon/>
+                            </Box>
+                            <Box style={{display:"flex", flexDirection:"column",width:"60%", height:"100%",}}>
+                                <Input placeholder='Энд бичнэ үү'></Input>
+                                <Input  placeholder='Энд бичнэ үү'></Input>
+                                <Input  placeholder='Энд бичнэ үү'></Input>
+                            </Box>
+                        </Box>
+                    </Box>
+
+                </Modal>
+            </Box>
+
         </Box>
     )
 }
--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -23,22 +23,8 @@ import SearchIcon from '@mui/icons-material/Search';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import Logo from "../images/Logo.png"
 import FoodCart from "./FoodCart";
-import {Modal} from "@mui/material";
-import { useFunction } from "../provider/Function";
-
-const style = {
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
   const  Home = () => {
-    const {open, handleClose, handleOpen} = useFunction();
   return (
     <Box sx={{ display: "flex" }}>
 
@@ -94,7 +80,7 @@ const style = {
               </ListItem>
               <Button sx={{width:"100%"}}  variant="contained" color="success">
                 Нэвтрэх
-              </Button>
+              </Button>         
           </List>
         </Drawer>
       </Box>
@@ -106,26 +92,7 @@ const style = {
           <FoodCart/>
           <FoodCart/>
           <FoodCart/>
-        <div>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
-              </Typography>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-              </Typography>
-            </Box>
-          </Modal>
-         </div>
         </Box>
-
-
     </Box>
   );
 }
