19f83851668c6b8162aa9c36a80613bda7699355
Tengis <tengis@iMac-20s-iMac.local>
2022-09-22 11:09:05 +0800

    firebase

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,15 +1,9 @@
 import * as React from "react";
-import {
-  Box,
-  Toolbar, 
-  Typography, 
-  Avatar,
-} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import {Box} from "@mui/material";
 import FoodCart from "./FoodCart";
-import NavBar from "./navBar";
+import SideBar from "./SideBar";
 import {useHookTest} from "../customHook/getDocCustomHook";
+import NavBar from "./NavBar";
 
 
   const  Home = () => {
@@ -18,30 +12,8 @@ import {useHookTest} from "../customHook/getDocCustomHook";
 
   return (
     <Box sx={{ display: "flex" }}>
-
-      {/* Header heseg */}
-
-      <Box sx={{width:"8vw"}}>
-        <Box
-          position="fixed"
-          sx={{ width: `calc(100% - 240px)`, ml: `240px`}}
-          >
-
-          <Toolbar sx={{height:"70px", display:"flex", alignItems:"center", justifyContent:"space-between", color:"black", backgroundColor:"white", borderBottom:"1px solid silver"}}>
-            <Typography variant="h5">Захиалах</Typography>
-            <Box Item sx={{ display:"flex", alignItems:"center", justifyContent:"space-around",width:"13vw"}}>
-              <SearchIcon/>
-              <NotificationsActiveIcon/>
-              <Box sx={{fontSize:"30px", color:"silver"}}>|</Box>
-              <Typography variant="h5">Tengis</Typography>
-              <Avatar/>
-            </Box>
-          </Toolbar>
-
-        </Box>
-      </Box>
-
-        <NavBar/>
+      <NavBar/>
+      <SideBar/>
 
       <Box sx={{height:"auto%", width:"92vw",marginTop:"70px", backgroundColor:"red", display:"flex",flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-between"}}>
           <FoodCart/>
--- src/HomePage/navBar.jsx
+++ src/HomePage/navBar.jsx
@@ -1,62 +1,28 @@
-import SettingsIcon from '@mui/icons-material/Settings'; 
-import EqualizerIcon from '@mui/icons-material/Equalizer';
-import DescriptionIcon from '@mui/icons-material/Description';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
-import{
-    Drawer, 
-    List,
-    ListItem, 
-    ListItemText,
-    Button,
-    Box,
-} from "@mui/material"
-import Logo from "../images/Logo.png"
-import LoginModal from './LogInModal';
-import { useFunction } from '../provider/FunctionProvider';
+import { Box, Toolbar, Avatar, Typography } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 
 const NavBar = () => {
+return(
 
-    const {handleOpen} = useFunction();
+    <Box sx={{width:"8vw"}}>
+        <Box
+        position="fixed"
+        sx={{ width: `calc(100% - 240px)`, ml: `240px`}}
+        >
 
-    return(
-      <Box>
-        <Drawer
-          sx={{
-            width:"40px",
-            "& .MuiDrawer-paper": {
-              width: 240,
-              backgroundColor:"#000723",
-              paddingTop:"40px",
-              width:"10vw"
-            },
-            }}
-            variant="permanent"
-            anchor="left">
-          <img style={{width:"126px", height:"55.17px", marginLeft:"50px", marginBottom:"20px"}} src={Logo}/>
-          <List sx={{backgroundColor:"#000723", color:"white"}} component="nav" aria-label="mailbox folders">
-              <ListItem button>
-                <DescriptionIcon/>
-                <ListItemText primary="Захиалга" />
-              </ListItem>
-              <ListItem button divider>
-                <EqualizerIcon/>
-                <ListItemText primary="График" />
-              </ListItem>
-              <ListItem button>
-                  <SettingsIcon/>
-                <ListItemText primary="Тохиргоо" />
-              </ListItem>
-              <ListItem button>
-                <RestaurantIcon/>
-                <ListItemText primary="Меню" />
-              </ListItem>
-              <Button onClick={handleOpen} sx={{width:"100%"}}  variant="contained" color="success">
-                Нэвтрэх
-              </Button>   
-              <LoginModal/>      
-          </List>
-        </Drawer>
-      </Box>
+        <Toolbar sx={{height:"70px", display:"flex", alignItems:"center", justifyContent:"space-between", color:"black", backgroundColor:"white", borderBottom:"1px solid silver"}}>
+            <Typography variant="h5">Захиалах</Typography>
+            <Box Item sx={{ display:"flex", alignItems:"center", justifyContent:"space-around",width:"13vw"}}>
+            <SearchIcon/>
+            <NotificationsActiveIcon/>
+            <Box sx={{fontSize:"30px", color:"silver"}}>|</Box>
+            <Typography variant="h5">Tengis</Typography>
+            <Avatar/>
+            </Box>
+        </Toolbar>
+        </Box>
+    </Box>
     )
 }
 export default NavBar;
